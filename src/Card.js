84e80a7fb402c812c4514a018eb7d2b0105e93f4
Adeola <meetprecious1403@gmail.com>
2022-10-31 12:52:08 +0100

    add mui

---
+++ src/Card.js
@@ -0,0 +1,8 @@
+import {Box} from "@mui/material";
+import Profile from "./Profile"
+
+export default function Card (){
+    return <Box>
+    <Profile /> 
+    </Box>
+}
---
+++ src/Profile.js
@@ -0,0 +1,9 @@
+import {Avatar} from "@mui/material"
+
+export default function Profile(){
+  return <div>
+     <Avatar alt="my image" src= ""/>
+  <h2>Adextra01</h2>
+  </div>
+}
+ 
