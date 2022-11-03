28490e9052405d3352ca471bea5a948d3d0b7cfc
Adeola <meetprecious1403@gmail.com>
2022-11-03 12:25:20 +0100

    style: improve top button appearance on mobile

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -2,7 +2,7 @@ import slack from "./slack.png"
 import github from "./github_icon.png"
 export default function Socials(){
     return <div className="btm-nav">
-        <a target="_blank" rel="noopener noreferrer" href=""><img alt="slack_img" src= {slack}/></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://slack.com/Adeola Samuel"><img alt="slack_img" src= {slack}/></a>
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/Adeola-creator"><img alt="github_img" src= {github} /></a>
     </div>
 }
