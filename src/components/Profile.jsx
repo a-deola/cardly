6e8424db9f54431585c3239712cd7633a146c0ce
Adeola <meetprecious1403@gmail.com>
2022-11-07 16:59:32 +0100

    create components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+
 import { Avatar, Card } from "@mui/material"
 import share from "./share_icon.png"
 import mobile from "./mobile_icon.png"
@@ -6,10 +6,10 @@ import mobile from "./mobile_icon.png"
 export default function Profile() {
   return <Card variant="none" className="profile">
     <picture>
-      <source media="(max-width: 600px)" srcset={mobile} />
+      <source media="(max-width: 600px)" srcSet={mobile} />
       <img className= "share" src={share} alt="share_btn" />
     </picture>
-    <Avatar id="profile_img" alt="profile_img" src="https://res.cloudinary.com/dcuvw51mi/image/upload/ar_1:1,b_rgb:262c35,bo_5px_solid_rgb:070000,c_fill,g_auto,r_max,w_1000/v1667376995/IMG_0074_gzssq5.jpg" />
+      <Avatar sx={{ width: 60, height: 60 }} id="profile_img" alt="profile_img" src="https://res.cloudinary.com/dcuvw51mi/image/upload/ar_1:1,b_rgb:262c35,bo_5px_solid_rgb:070000,c_fill,g_auto,r_max,w_1000/v1667376995/IMG_0074_gzssq5.jpg" />
     <h5 id="twitter">Adextra01</h5>
     <h5 id="slack">Adeola Samuel</h5>
   </Card>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,8 +1,8 @@
-import slack from "./slack.png"
+import discord from "./discord.png"
 import github from "./github_icon.png"
 export default function Socials(){
     return <div className="btm-nav">
-        <a target="_blank" rel="noopener noreferrer" href="https://app.slack.com/client/T042F7V19Q8/C041S7VF5JR/rimeto_profile/U048K3B6MJR"><img alt="slack_img" src= {slack}/></a>
+        <a target="_blank" rel="noopener noreferrer" href="#"><img alt="discord_img" src= {discord}/></a>
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/Adeola-creator"><img alt="github_img" src= {github} /></a>
     </div>
 }
