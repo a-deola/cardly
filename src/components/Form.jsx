ae6a5d1031210c671d954be8b0a8f00f80d19de5
Adeola <meetprecious1403@gmail.com>
2022-11-07 17:02:26 +0100

    create form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import { Button, TextField } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+export default function Form() {
+    return <form className="my-form" action="mailto:meetprecious1403@gmail.com" method="post" encType="text/plain">
+        <div className="names">
+            <label className="form-fields mobile-text">
+                First name
+                <input className="my-inputs" id="fName" type="text" placeholder="Enter your first name" />
+            </label>
+
+            <label className="form-fields right-link mobile-text">
+                Last name
+                <input className="my-inputs right-link " type="text" placeholder="Enter your last name" />
+            </label>
+        </div>
+
+        <label className="form-fields mobile-text">
+            Email
+            <input className="my-inputs" type="email" placeholder="yourname@email.com" />
+        </label>
+        <label className="form-fields mobile-text">
+            Message
+            <TextField  multiline rows={4} InputProps= {{ className: "moblie-text" }} margin="dense" label="Send a message" placeholder="Send me a message and I'll reply you as soon as possible..." />
+        </label>
+        <div className="my-check">
+        <label className="mobile-text">
+        <input type="checkbox" />
+               You agree to providing your data to <span>Adeola</span> who may contact you.
+        </label>
+        </div>
+       
+        <Button sx={{ textTransform: "none",mb: 2}} variant="contained" endIcon={<SendIcon />}>Send message</Button>
+    </form>
+}
---
+++ src/components/contact-intro.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Intro(){
+    return <div>
+        <h2 className="contact-heading">Contact Me</h2>
+        <p className="intro mobile-text">Hi there, contact me to ask me about anything you have in mind.</p>
+    </div>
+}
