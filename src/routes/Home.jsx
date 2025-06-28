7f07f768b95e327e6ec9c9c5b5d7a73e3a2dd18c
Adeola Samuel <meetprecious1403@gmail.com>
2025-06-28 10:43:06 +0100

    space footer

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,6 @@ import Profile from "../components/Profile";
 import Links from "../content";
 import ListItem from "../components/ListItem";
 import Footer from "../components/Footer";
-import { Grid } from "@mui/material";
 
 export default function Main() {
   return (
